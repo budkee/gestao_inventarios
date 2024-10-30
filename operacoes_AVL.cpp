d3feb853f6cbdf720684608632591e8c50d98c4d
budkee <kae.budke@gmail.com>
2024-10-29 22:01:34 -0400

    Organização inicial

---
+++ operacoes_AVL.cpp
@@ -0,0 +1,20 @@
+/**************************************************
+ *
+ * Isabelle Bicudo e Kaê de Oliveira Budke
+ * Trabalho 2
+ * Professor: Fábio Henrique Viduani Martinez
+ *
+ */
+
+/*
+
+    // ------------- Operações a serem implementadas ------------- //
+    Op. Básicas:
+    - busca
+    - inserção
+    - remoção
+    Op. Avançadas:
+    - união de árvores AVL
+    - interseção de árvores AVL
+    - busca de elementos em um intervalo
+*/
